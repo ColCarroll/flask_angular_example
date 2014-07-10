0c7585d417c45c1697202db2785dd549143e3fe5
David Brear <david.brear@tabbedout.com>
2014-07-09 20:54:04 -0500

    moved the api call to query params

--- angular_example.py
+++ angular_example.py
@@ -1,7 +1,7 @@
 import os
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 def get_model():
     iris = datasets.load_iris()
-    return RandomForestClassifier().fit(iris.data, iris.target), list(iris.target_names)
+    return RandomForestClassifier(n_estimators=100).fit(iris.data, iris.target), list(iris.target_names)
 
 
 MODEL, LABELS = get_model()
@@ -19,10 +19,14 @@ MODEL, LABELS = get_model()
 def index():
     return make_response(open(os.path.join(DIR, 'templates/index.html')).read())
 
-
-@app.route('/_model/<float:a>/<float:b>/<float:c>/<float:d>')
-def echo(a, b, c, d):
-    probs = MODEL.predict_proba([a, b, c, d])[0]
+# _model/<float:a>/<float:b>/<float:c>/<float:d>
+@app.route('/api/predict')
+def echo():
+    a = request.args.get('sepal_length')
+    b = request.args.get('sepal_width')
+    c = request.args.get('petal_length')
+    d = request.args.get('petal_width')
+    probs = MODEL.predict_proba(map(float, [a, b, c, d]))[0]
     val = {LABELS[j]: probs[j] for j in range(len(probs))}
     return jsonify(val)
 
