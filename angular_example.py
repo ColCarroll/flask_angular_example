9948a63eceda388569c8e17d618599e9b36e9bb5
Colin Carroll <colinc@spiceworks.com>
2014-07-12 16:07:41 -0500

    Added d3js chart

--- angular_example.py
+++ angular_example.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 def get_model():
     iris = datasets.load_iris()
-    return RandomForestClassifier(n_estimators=100).fit(iris.data, iris.target), list(iris.target_names)
+    return RandomForestClassifier(n_estimators=1000).fit(iris.data, iris.target), list(iris.target_names)
 
 
 MODEL, LABELS = get_model()
@@ -19,7 +19,7 @@ MODEL, LABELS = get_model()
 def index():
     return make_response(open(os.path.join(DIR, 'templates/index.html')).read())
 
-# _model/<float:a>/<float:b>/<float:c>/<float:d>
+
 @app.route('/api/predict')
 def echo():
     a = request.args.get('sepal_length')
@@ -27,7 +27,7 @@ def echo():
     c = request.args.get('petal_length')
     d = request.args.get('petal_width')
     probs = MODEL.predict_proba(map(float, [a, b, c, d]))[0]
-    val = {LABELS[j]: probs[j] for j in range(len(probs))}
+    val = {"data": [{"label": label, "prob": prob} for label, prob in zip(LABELS, probs)]}
     return jsonify(val)
 
 
--- static/app.js
+++ static/app.js
@@ -1,43 +1,135 @@
 /**
  * Created by colinc on 6/25/14.
  */
-function ModelCtrl($scope, $http) {
-  $scope.features = {
-    sepalLength : 1.1,
-    sepalWidth : 1.1,
-    petalLength : 1.1,
-    petalWidth : 1.2
-  };
 
-  $scope.getGraph = function() {
+var flaskAngularApp = angular.module("flaskAngularApp", []);
 
-    return "hello";
-  };
+flaskAngularApp.controller("ModelCtrl", function ModelCtrl($scope, $http) {
+    $scope.features = {
+        sepalLength : 4.1,
+        sepalWidth : 5.0,
+        petalLength : 2.5,
+        petalWidth : 3
+    };
 
-  $scope.url = function() {
-    return("/api/predict?" +
-      "sepal_length=" + this.features.sepalLength.toFixed(2) + "&" +
-      "sepal_width=" + this.features.sepalWidth.toFixed(2) + "&" +
-      "petal_length=" + this.features.petalLength.toFixed(2) + "&" +
-      "petal_width=" + this.features.petalWidth.toFixed(2));
-  };
+    $scope.url = function() {
+        return("/api/predict?" +
+            "sepal_length=" + this.features.sepalLength.toFixed(1) + "&" +
+            "sepal_width=" + this.features.sepalWidth.toFixed(1) + "&" +
+            "petal_length=" + this.features.petalLength.toFixed(1) + "&" +
+            "petal_width=" + this.features.petalWidth.toFixed(1));
+    };
 
-  $scope.probs = {};
+    $scope.probs = {};
 
-  $scope.callIt = function() {
-    $http({
-    method: 'GET',
-    url: $scope.url()
-    }).success(function(data){
-        $scope.probs = data;
-    });
-  };
+    $scope.callIt = function() {
+        $http({
+            method: 'GET',
+            url: $scope.url()
+        }).success(function(data){
+            $scope.probs = data.data;
+        });
+    };
 
-  $scope.getProbs = function(){
-      return $scope.probs;
-  };
+    $scope.getProbs = function(){
+        return $scope.probs;
+    };
 
-  $scope.callIt();
+    $scope.callIt();
 
 
-}
+});
+
+flaskAngularApp.directive("irisBars", function(){
+
+    // constants
+    var margin = 20,
+        width = 450 - margin,
+        height = 300,
+        yScale = d3.scale.linear()
+            .domain([0, 1])
+            .range([0, height]),
+        barPadding = 3,
+        duration = 500;
+
+    return {
+        restrict: "E",
+        scope: {
+            data: '='
+        },
+        link: function (scope, element, attrs) {
+
+            // set up initial svg object
+            var svg = d3.select(element[0])
+                .append("svg")
+                .style("width", width)
+                .attr("height", height + margin + 100);
+
+            scope.$watch("data", function (data) {
+                scope.render(data);
+            });
+
+            scope.render = function(newData){
+                if (!newData || isNaN(newData.length)) return;
+
+                var bars = svg.selectAll("rect")
+                    .data(newData, function(d) { return d.label; });
+
+                var text = svg.selectAll("text")
+                    .data(newData, function(d) { return d.label; });
+
+                var barWidth = width / newData.length;
+
+                bars
+                    .transition()
+                    .duration(duration)
+                    .attr("height", function(d){return yScale(d.prob)})
+                    .attr("y", function(d) {return height - Math.round(margin / 2) - yScale(d.prob)});
+
+                text
+                    .text(function(d) {return (100 * d.prob).toFixed(1) + "%";})
+                    .transition()
+                    .duration(duration)
+                    .attr("y", function(d) {return height - Math.round(margin / 2) - yScale(d.prob)});
+
+                bars
+                    .enter()
+                    .append('rect')
+                    .attr("width", barWidth)
+                    .attr("fill", "steelblue")
+                    .attr("x", function(d, i) { return i * (barWidth + barPadding) })
+                    .data(newData)
+                    .transition()
+                    .duration(duration)
+                    .attr("height", function(d){return yScale(d.prob)})
+                    .attr("y", function(d) {return height - Math.round(margin / 2) - yScale(d.prob)});
+
+                text
+                    .enter()
+                    .append("text")
+                    .attr("x", function(d, i) { return (i + 1) * (barWidth + barPadding) })
+                    .attr("dx", -barWidth/2)
+                    .attr("dy", "1.2em")
+                    .attr("text-anchor", "middle")
+                    .text(function(d) {return (100 * d.prob).toFixed(1) + "%";})
+                    .attr("fill", "white")
+                    .transition()
+                    .duration(duration)
+                    .attr("y", function(d) {return height - Math.round(margin / 2) - yScale(d.prob)});
+
+                text
+                    .enter()
+                    .append("text")
+                    .attr("y", height)
+                    .attr("x", function(d, i) { return (i + 1) * (barWidth + barPadding) })
+                    .attr("dx", -barWidth/2)
+                    .attr("text-anchor", "middle")
+                    .attr("fill", "black")
+                    .text(function(d) { return d.label;});
+
+            };
+
+
+        }
+    }
+});
